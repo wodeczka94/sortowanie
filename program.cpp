9d2f29212d557d5eaa20b8d6749ab35b811f9e59
rafalgrabinski94 <rafalgrabinski94@gmail.com>
2017-03-22 16:30:14 +0100

    Program liczby

---
+++ program.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <iterator>
+
+int sortowanie()  
+{
+	std::ifstream in;
+  std::ofstream out;
+
+  int max = 100; //30
+  int min = 0;
+  int i;
+  int licznik;
+  int* A = new int[max];
+
+  in.open("liczby.txt");
+
+  
+  while(in >> i) {
+    
+    if(min == max) {
+    //	int* B = new int[2];
+      int* B = new int[2*max];
+      for(licznik = 0;licznik < min;licznik++)
+        B[licznik] = A[licznik];
+      delete[] A;
+      A = B;
+      max *= 2;
+    }
+    A[min] = i;
+    min++;
+  }
+  in.close();
+	
+  for(int min = 1;min < max;min++) {
+    int zmienna = A[min];
+    int licznik = min;
+    while(licznik > 0) {
+      if(A[licznik-1] < zmienna) {
+        A[licznik] = A[licznik-1];
+        licznik--;
+      } else
+        break;
+    }
+    A[licznik] = zmienna;
+  }
+  
+  out.open("liczby_mal.txt");
+  for(licznik = 0;licznik < min;licznik++)
+    out << A[licznik] << std::endl;
+  out.close();
+}
+
+int main()
+{
+sortowanie();
+return 0;
+}
+
+
