6addc6859e62d3fe98c480ee9a68b1ed82b81268
pawel_wojewoda <wodeczka94@gmail.com>
2017-03-22 17:04:33 +0100

    Poprawka

--- program.cpp
+++ program.cpp
@@ -4,7 +4,9 @@
 #include <algorithm>
 #include <set>
 #include <iterator>
-
+#include <stdio.h>
+#include <string.h>
+ 
 using namespace std ;
 
 int nazwisko()
@@ -14,56 +16,66 @@ ifstream in("nazwiska.txt");
 ofstream out("nazwiska_leksyko.txt");
 copy(istream_iterator<string>(in),istream_iterator<string>(),inserter(s,s.begin()));
 copy(s.begin(),s.end(),ostream_iterator<string>(out,"\n"));
-
-int sortowanie()  
-{
-	std::ifstream in;
-  std::ofstream out;
-
-  int max = 100; //30
-  int min = 0;
-  int i;
-  int licznik;
-  int* A = new int[max];
-
-  in.open("liczby.txt");
-
-  
-  while(in >> i) {
-    
-    if(min == max) {
-    //	int* B = new int[2];
-      int* B = new int[2*max];
-      for(licznik = 0;licznik < min;licznik++)
-        B[licznik] = A[licznik];
-      delete[] A;
-      A = B;
-      max *= 2;
-    }
-    A[min] = i;
-    min++;
-  }
-  in.close();
-	
-  for(int min = 1;min < max;min++) {
-    int zmienna = A[min];
-    int licznik = min;
-    while(licznik > 0) {
-      if(A[licznik-1] < zmienna) {
-        A[licznik] = A[licznik-1];
-        licznik--;
-      } else
-        break;
-    }
-    A[licznik] = zmienna;
-  }
-  
-  out.open("liczby_mal.txt");
-  for(licznik = 0;licznik < min;licznik++)
-    out << A[licznik] << std::endl;
-  out.close();
 }
+void sortowanie( )
+{
+  FILE *plik;
+  char strtmp[ 501 ];
+  char nazwisko[ 501 ];
+  int res;
+  int index;
+  char Nazwiska[100][501];
+  int i,j;
 
+  plik = fopen("liczby.txt", "rt");
+  if(plik==NULL)
+  {
+    printf("B31d odczytu pliku\n");
+    return;
+  }
+  index=0;
+  while((!feof(plik))&&(index<100))
+  {
+   
+    res=fscanf(plik,"%s",nazwisko);
+    if(res!=1)
+    {
+      printf("B31d odczytu");
+      break;
+    }
+    strcpy(Nazwiska[index],nazwisko);
+    index++;
+  }
+  fclose( plik);
+  for(i=0;i<index;i++)
+  {
+    for(j=0;j<index-i-1;++j)
+    {
+      if(strcmp(Nazwiska[j],Nazwiska[j-1])>0)
+      {
+    
+        strcpy(strtmp,Nazwiska[j]);
+        strcpy(Nazwiska[j],Nazwiska[j-1]);
+        strcpy(Nazwiska[j-1],strtmp);
+      }
+    }
+  }
+
+  plik = fopen("liczby_mal.txt", "w+t");
+  if(plik==NULL)
+  {
+    printf("B31d otwarcia pliku\n");
+    return ;
+  }
+  for(i=0;i<index;i++)
+  {
+    fprintf(plik,"%s ",Nazwiska[i]);
+    fprintf(plik,"\n");
+  }
+  fclose( plik );
+
+  return;
+}
 int main()
 {
 nazwisko();
